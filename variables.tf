9c59e18d9ebaa7021a8f4b646ce41842ea800430
kwameconnect <kwamequainoo@gmx.de>
2025-09-08 20:43:43 +0000

    Update variables.tf
    populated variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,25 @@
+# /iac/variables.tf
+variable "redshift_db_name" {
+    description = "Redshift database name"
+    type = string
+    default = "weather_db"
+}
+
+variable "redshift_admin_username" {
+    description = "Admin username for Redshift"
+    type = string 
+    default = "admin"
+}
+
+variable "redshift_admin_password" {
+    description = "Admin password for Redshift" 
+    type = string
+    sensitive = true
+}
+
+variable "visual_crossing_api_key" {
+    description = "Visual Crossing API key"
+    type = string
+    sensitive = true
+}
 
